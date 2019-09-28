b0fb48e1cac74f6b32360018baf369104b1582c3
OsengKangkung <53109848+OsengKangkung@users.noreply.github.com>
2019-09-28 21:34:36 +0700

    soal1

---
+++ soal_1.php
@@ -0,0 +1,38 @@
+<?php
+	<?php
+    function biodata() {
+
+        $name = 'alwan'; $age= 21;
+
+        $biodata = [
+            'name' => 'alwan',
+            'age' => 21,
+            'address' => 'mayong, jepara',
+            'hobbies' => ['football', 'badminton'],
+            'is_maried' => false,
+            'list_school' => (object) [
+                'name' => 'smk gatoloco',
+                'years_in' => 2013,
+                'years_out' => 2016,
+                'major' => 'tkj',
+            ],
+            'skills' => (object) [
+                'skill' => 'football',
+                'level' => 'advenced',
+
+                'skill' => 'badminton',
+                'level' => 'beginner',
+            ],
+
+            'interest_in_coding' => true
+
+        ];
+
+        echo json_encode($biodata);
+
+    }
+
+
+    biodata();
+?>
+?>
